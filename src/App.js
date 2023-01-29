a7c4d41786d4c8a2f7030923dea6c6196bb7b053
Singamsai <103039188+Singamsai@users.noreply.github.com>
2023-01-29 17:37:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {ItemList} from './components/ItemList';
+import {Item}  from './components/Item';
+
+function App() {
+  return (
+    <div className="App">
+    <Item></Item>
+    <ItemList></ItemList>  
+    </div>
+  )
+}
+
+export default App;
